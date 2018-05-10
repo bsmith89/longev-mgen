4c7d5807774799bf0d7cee8b5862b2c480b0287f
Byron Smith <me@byronjsmith.com>
2018-05-10 16:35:54 -0400

    Update CAZy analysis to match master Snakefile.

--- scripts/parse_hmmer_output.py
+++ scripts/parse_hmmer_output.py
@@ -5,12 +5,12 @@ from Bio.SearchIO import parse
 import sys
 
 if __name__ == "__main__":
-    print('hit_id', 'hit_description', 'query_id', 'hit_start', 'hit_end',
+    print('hit_id', 'query_id', 'hit_start', 'hit_end',
           'query_start', 'query_end', 'bitscore', 'included', sep='\t')
     recs = parse(sys.stdin, 'hmmer3-text')
     for model_hits in recs:
         for hsp in model_hits.hsps:
-            print(hsp.hit_id, hsp.hit_description,
+            print(hsp.hit_id,
                   hsp.query_id,
                   hsp.hit_start, hsp.hit_end,
                   hsp.query_start, hsp.query_end,

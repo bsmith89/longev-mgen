a8d9f7ed61014e794fe3e5c3af67f1079fc19d95
Byron Smith <me@byronjsmith.com>
2018-05-16 17:40:44 -0400

    Correct buggy rrs query.

--- scripts/query_longev_rrs_count.sql
+++ scripts/query_longev_rrs_count.sql
@@ -6,9 +6,9 @@ SELECT
   , taxon_id_b AS otu_id
   , SUM(tally)
 FROM rrs_library
-JOIN mgen_library USING (extraction_id)
 JOIN _rrs_library_taxon_count USING (rrs_library_id)
 JOIN taxonomy USING (taxon_id, taxon_level)
+JOIN extraction USING (extraction_id)
 WHERE taxon_level = 'unique'
 GROUP BY extraction_id, sequence_id
 ;

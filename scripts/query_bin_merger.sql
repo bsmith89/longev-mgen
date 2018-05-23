78ac4a5c3aa2aa439aa74b7b72ff8638c893cdc1
Byron Smith <me@byronjsmith.com>
2018-05-23 11:20:21 -0400

    Additional experimental linkage indices.

--- scripts/query_bin_merger.sql
+++ scripts/query_bin_merger.sql
@@ -1,5 +1,8 @@
 SELECT
     b.*
+  , 1.0 * b.read_count / (n1.length + n2.length) AS linkage_score_1
+  , (b.read_count * b.read_count) / (c1.coverage * c2.coverage) / (n1.length + n2.length) AS linkage_score_2
+  , (b.read_count * b.read_count) / (c1.coverage * c2.coverage) AS linkage_score_3
   , n1.n_contigs AS ncontigs_1
   , n2.n_contigs AS ncontigs_2
   , n1.length AS length_1
@@ -17,5 +20,5 @@ JOIN bin_checkm AS d2 ON d2.bin_id = bin_id_2
 JOIN (SELECT bin_id, SUM(coverage) AS coverage FROM bin_coverage GROUP BY bin_id) AS c1 ON c1.bin_id = bin_id_1
 JOIN (SELECT bin_id, SUM(coverage) AS coverage FROM bin_coverage GROUP BY bin_id) AS c2 ON c2.bin_id = bin_id_2
 LEFT JOIN bin_complementarity AS m ON m.bin_id = bin_id_1 AND m.bin_id_combined = bin_id_2
-ORDER BY read_count DESC, complementarity_score DESC
+ORDER BY linkage_score_1 DESC, complementarity_score DESC
 ;

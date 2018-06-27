b1ce0215232fcbf0607184f7832f52ddae234565
Byron Smith <me@byronjsmith.com>
2018-06-27 09:33:23 -0400

    More explicit and efficient splitting function.
    Also fix a bug (that may have never been realized) with propagating offsets.

--- scripts/depth_trim_contigs.py
+++ scripts/depth_trim_contigs.py
@@ -17,43 +17,50 @@ def depth_trim(depth, thresh, flank, window, offset=0):
     left_most = None
     right_most = None
     depth = np.asarray(depth)
-    # Trim in from the left.
+    # Trim flanks in from the left.
     for i in range(0, len(depth) - flank + 1):
         left = i
         right = i + flank
         if depth[left:right].mean() > thresh:
             left_most = i
             break
-    # Trim in from the right.
+    if left_most is None:
+        # We didn't find any part of the sequence with enough depth.
+        return []
+
+    # Trim flanks in from the right.
     for i in range(len(depth), flank - 1, -1):
         left = i - flank
         right = i
         if depth[left:right].mean() > thresh:
             right_most = i
             break
-
-    if (left_most is None) or (right_most is None):
+    if right_most is None:
         # We didn't find any part of the sequence with enough depth.
         return []
-    elif (right_most - left_most) < window:
+
+    if (right_most - left_most) < window:
         # The sequence is too short to try and split.
-        return [(left_most, right_most)]
+        return [(left_most + offset, right_most + offset)]
+
+    # Find internal breakpoint and recursively call depth_trim.
+    break_at = None
+    for i in range(left_most + window // 2, right_most - window // 2):
+        left = i - window // 2
+        right = i + window // 2
+        if depth[left:right].mean() < thresh:
+            # Found the first breakpoint scanning from the left.
+            break_at = i
+            break
+    if break_at is None:
+        # Found no internal breakpoints.
+        return [(left_most + offset, right_most + offset)]
     else:
-        # Find internal breakpoint and recursively call depth_trim.
-        break_at = None
-        for i in range(left_most + window // 2, right_most - window // 2):
-            left = i - window // 2
-            right = i + window // 2
-            if depth[left:right].mean() < thresh:
-                break_at = i
-        if break_at is None:
-            # Found no internal breakpoints.
-            return [(left_most + offset, right_most + offset)]
-        else:
-            # Found internal breakpoints recursively; concatenate and return.
-            left_frags_trimmed = depth_trim(depth[left_most:break_at], thresh, flank, window, offset=left_most + offset)
-            right_frags_trimmed = depth_trim(depth[break_at:right_most], thresh, flank, window, offset=break_at + offset)
-            return left_frags_trimmed + right_frags_trimmed
+        # Found internal breakpoints recursively.
+        # Concatenate and return.
+        left_frags_trimmed = depth_trim(depth[left_most:break_at], thresh, flank, window, offset=left_most + offset)
+        right_frags_trimmed = depth_trim(depth[break_at:right_most], thresh, flank, window, offset=break_at + offset)
+        return left_frags_trimmed + right_frags_trimmed
 
 
 if __name__ == "__main__":

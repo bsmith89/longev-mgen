e6ed2f503c9537aba93a07d4466b4913e5f09983
Byron Smith <me@byronjsmith.com>
2018-06-27 14:48:28 -0400

    Start writing a new script for MAG sequence filtering.

---
+++ scripts/calculate_per_position_correlations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import tqdm.tqdm.pandas as tqdm_pandas_register
+
+tqdm_pandas_register()
+
+if __name__ == "__main__":
+    pass

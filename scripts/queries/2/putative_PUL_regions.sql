2ac78a30aea135d0c0e6f83874f72e75f9d13d4f
Byron Smith <me@byronjsmith.com>
2018-09-25 15:40:34 -0400

    Display actual starch active domain hit.

--- scripts/queries/2/putative_PUL_regions.sql
+++ scripts/queries/2/putative_PUL_regions.sql
@@ -10,7 +10,7 @@ SELECT DISTINCT
   , susD
   , susEF
   , susG
-  , starch_active
+  , starch_active_domain
   , localization
   , opf_id
   , architecture
@@ -21,7 +21,7 @@ LEFT JOIN (SELECT feature_id, 1 AS susC FROM putative_susC) AS c USING (feature_
 LEFT JOIN (SELECT feature_id, 1 AS susD FROM putative_susD) AS d USING (feature_id)
 LEFT JOIN (SELECT feature_id, 1 AS susEF FROM putative_susEF) AS e USING (feature_id)
 LEFT JOIN (SELECT feature_id, 1 AS susG FROM putative_susG) AS g USING (feature_id)
-LEFT JOIN (SELECT DISTINCT feature_id, 1 AS starch_active
+LEFT JOIN (SELECT DISTINCT feature_id, domain_id AS starch_active_domain
            FROM starch_active_domain_best_hit
           ) AS s USING (feature_id)
 WHERE DISTANCE < 15000

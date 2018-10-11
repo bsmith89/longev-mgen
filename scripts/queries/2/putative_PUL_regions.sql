8ecb7cca6f2edc0bb69ab527bb37bc40d7f6cd97
Byron Smith <me@byronjsmith.com>
2018-10-11 19:52:03 -0400

    Update queries for new DB.

--- scripts/queries/2/putative_PUL_regions.sql
+++ scripts/queries/2/putative_PUL_regions.sql
@@ -1,36 +1,30 @@
--- Find all of the putative SusD features while being robust to annotation errors.
---
--- This is a pretty liberal filter.
-
 SELECT DISTINCT
     sequence_id
   , feature_id
-  , left
-  , right
+  , feature_start
+  , feature_stop
   , susC
   , susD
   , susEF
-  , susG
   , starch_active_domain
   , localization
   , opf_id
   , architecture
   , product_description
   , cazy_domain_list
-FROM feature_neighborhood
+FROM (SELECT feature_id AS seed_id FROM pul_susC)
+JOIN feature_distance USING (seed_id)
 JOIN feature_details USING (feature_id)
-LEFT JOIN (SELECT feature_id, 1 AS susC FROM putative_susC) AS c USING (feature_id)
-LEFT JOIN (SELECT feature_id, 1 AS susD FROM putative_susD) AS d USING (feature_id)
-LEFT JOIN (SELECT feature_id, 1 AS susEF FROM putative_susEF) AS e USING (feature_id)
-LEFT JOIN (SELECT feature_id, 1 AS susG FROM putative_susG) AS g USING (feature_id)
+LEFT JOIN (SELECT feature_id, 1 AS susC FROM susC) USING (feature_id)
+LEFT JOIN (SELECT feature_id, 1 AS susD FROM susD) USING (feature_id)
+LEFT JOIN (SELECT feature_id, 1 AS susEF FROM susEF) USING (feature_id)
 LEFT JOIN (SELECT DISTINCT feature_id, domain_id AS starch_active_domain
            FROM starch_active_domain_best_hit
-          ) AS s USING (feature_id)
+          ) USING (feature_id)
 LEFT JOIN (SELECT
                feature_id
              , GROUP_CONCAT(domain_id, ',') AS cazy_domain_list
            FROM feature_x_cazy_minimal_domain
-           GROUP BY feature_id) AS ca USING (feature_id)
+           GROUP BY feature_id) USING (feature_id)
 WHERE DISTANCE < 25000
-  AND seed_id IN (SELECT feature_id FROM putative_PUL_susC)
 ;

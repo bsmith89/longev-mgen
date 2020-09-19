6aa5e811e3400eb8a34b82824b1fd481c8f0c1a8
Byron Smith <me@byronjsmith.com>
2020-09-19 12:53:26 -0700

    Add some helpful info messages.

--- scripts/pick_marker_genes.py
+++ scripts/pick_marker_genes.py
@@ -4,11 +4,11 @@
 import pandas as pd
 from Bio.SeqIO import index as index_seqs
 import sys
-from collections import defaultdict
 import argparse
+from tqdm import tqdm
 
 
-def log(message):
+def info(message):
     print(message, file=sys.stderr, flush=True)
 
 
@@ -32,9 +32,11 @@ if __name__ == "__main__":
     for arg in args.hits:
         genome_id, hmmer_path, fasta_path = arg.strip().split(":")
         input_paths[genome_id] = (hmmer_path, fasta_path)
+    n_paths = len(input_paths)
 
     data = []
-    for genome_id in input_paths:
+    info(f"Reading {n_paths} hits tables.")
+    for genome_id in tqdm(input_paths):
         hmmer_path, _ = input_paths[genome_id]
         d = pd.read_table(
             hmmer_path, names=["feature_id", "model_id", "score"]
@@ -43,6 +45,7 @@ if __name__ == "__main__":
         data.append(d)
     data = pd.concat(data)
 
+    info("Counting gene occurences.")
     model_by_genome_counts = (
         data.groupby(["genome_id"])
         .apply(lambda d: d.model_id.value_counts())
@@ -50,16 +53,29 @@ if __name__ == "__main__":
     )
 
     is_single_copy = (model_by_genome_counts > 1).sum() == 0
-    is_ubiquitous = (model_by_genome_counts == 1).mean() >= args.min_frac
+    n_single_copy = is_single_copy.sum()
+    info(f"Found {n_single_copy} single-copy genes.")
+
+    model_frequency = (model_by_genome_counts == 1).mean()
+    is_ubiquitous = model_frequency >= args.min_frac
+    n_ubiquitous = is_ubiquitous.sum()
+    info(f"Found {n_ubiquitous} common (>= {args.min_frac:.1%}) genes.")
+
+    info(f"Of single-copy genes, here are the most commonly found:")
+    info(model_frequency[is_single_copy].sort_values(ascending=False).head(50))
 
     shared_single_copy_models = idxwhere(is_single_copy & is_ubiquitous)
+    n_shared_single_copy = len(shared_single_copy_models)
+    info(f"Found {n_shared_single_copy} shared-single-copy genes.")
 
     if args.list_output:
+        info(f"Writing genes list.")
         with open(args.list_output, "w") as f:
             for model_id in shared_single_copy_models:
                 print(model_id, file=f)
 
     if args.tsv_output:
+        info(f"Writing gene-to-genome map.")
         (
             data[["genome_id", "model_id", "feature_id"]].to_csv(
                 args.tsv_output, sep="\t", header=False, index=False
@@ -67,6 +83,7 @@ if __name__ == "__main__":
         )
 
     if args.fasta_output_template:
+        info(f"Writing sequences.")
         fasta_indexes = {}
         for genome_id in input_paths:
             fasta_path = input_paths[genome_id][1]

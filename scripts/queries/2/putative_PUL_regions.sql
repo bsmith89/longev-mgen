e8095f5e52a2a90de6751062a9e0c29807a0e836
Byron Smith <me@byronjsmith.com>
2020-09-22 16:45:56 -0700

    Update paper figures.

--- scripts/queries/2/putative_PUL_regions.sql
+++ scripts/queries/2/putative_PUL_regions.sql
@@ -9,25 +9,27 @@ SELECT DISTINCT
   , susD
   , susEF
   , starch_active_domain
+  , starch_active_domain_score
   , localization
   , opf_id
   , architecture
   , product_description
   , cazy_domain_list
-FROM (SELECT feature_id AS seed_id FROM pul_susC)
-JOIN feature_distance USING (seed_id)
+FROM (SELECT feature_id FROM closest_PUL_susC WHERE distance < 25000)
 JOIN feature_details USING (feature_id)
 LEFT JOIN (SELECT feature_id, 1 AS susC FROM susC) USING (feature_id)
 LEFT JOIN (SELECT feature_id, 1 AS susD FROM susD) USING (feature_id)
 LEFT JOIN (SELECT feature_id, 1 AS susEF FROM susEF) USING (feature_id)
-LEFT JOIN (SELECT DISTINCT feature_id, domain_id AS starch_active_domain
+LEFT JOIN (SELECT DISTINCT
+               feature_id
+             , domain_id AS starch_active_domain
+             , score AS starch_active_domain_score
            FROM starch_active_domain_best_hit
           ) USING (feature_id)
 LEFT JOIN (SELECT
                feature_id
              , GROUP_CONCAT(domain_id, ',') AS cazy_domain_list
-           FROM feature_x_cazy_minimal_domain
+           FROM (SELECT * FROM feature_x_cazy_minimal_domain)
            GROUP BY feature_id) USING (feature_id)
 JOIN sequence USING (sequence_id)
-WHERE DISTANCE < 25000
 ;
